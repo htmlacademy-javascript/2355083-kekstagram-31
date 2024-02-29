ed41cf77a1701101332bc45343796c205cbc3536
Andrey <you@example.com>
2024-02-29 23:49:12 +0300

    functions_bug_fix

--- js/functions.js
+++ js/functions.js
@@ -1,35 +1,43 @@
-const checkLength=(str, len) =>str.length <= len ? true:false;
-
-console.log(checkLength('проверяемая строка', 20));
-console.log(checkLength('проверяемая строка', 18));
-console.log(checkLength('проверяемая строка', 10));
-
-
-
-const checkPolindrom = (str) => {
-	str = str.toLowerCase().replaceAll(' ', '')
-	for (let i = 0; i < Math.floor(str.length / 2); i++) {
-		if (str[i] !== str[(str.length - 1) - i]) return false;
-	}
-	return true;
+function checkLength(str, len) {
+  if (str.length <= len) {
+    return true;
+  }
+  return false;
 }
 
-console.log(checkPolindrom(' A bc Ed e  Cb a   '));
-console.log(checkPolindrom('топот'));
-console.log(checkPolindrom('ДовОд'));
-console.log(checkPolindrom('Кекс'));
-console.log(checkPolindrom('Лёша на полке клопа нашёл '));
+checkLength('проверяемая строка', 20);
+checkLength('проверяемая строка', 18);
+checkLength('проверяемая строка', 10);
 
-
-const ejectDigit = (data) => {
-	if (typeof (data) == "number") return Math.abs(data);
-	if (data.match(/[0-9]/g) !== null) return Number(data.match(/[0-9]/g).join(''));
-	return NaN;
+function checkPolindrom(str){
+  str = str.toLowerCase().replaceAll(' ', '');
+  for (let i = 0; i < Math.floor(str.length / 2); i++) {
+    if (str[i] !== str[(str.length - 1) - i]) {
+      return false;
+    }
+  }
+  return true;
 }
 
-console.log(ejectDigit('2023 год'));
-console.log(ejectDigit('ECMAScript 2022'));
-console.log(ejectDigit('1 кефир, 0.5 батона'));
-console.log(ejectDigit('агент 007'));
-console.log(ejectDigit('а я томат'));
-console.log(ejectDigit(-0));
+checkPolindrom(' A bc Ed e  Cb a   ');
+checkPolindrom('топот');
+checkPolindrom('ДовОд');
+checkPolindrom('Кекс');
+checkPolindrom('Лёша на полке клопа нашёл ');
+
+function ejectDigit(data){
+  if (typeof (data) === 'number') {
+    return Math.abs(data);
+  }
+  if (data.match(/[0-9]/g) !== null) {
+    return Number(data.match(/[0-9]/g).join(''));
+  }
+  return NaN;
+}
+
+ejectDigit('2023 год');
+ejectDigit('ECMAScript 2022');
+ejectDigit('1 кефир, 0.5 батона');
+ejectDigit('агент 007');
+ejectDigit('а я томат');
+ejectDigit(-0);
